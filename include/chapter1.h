cdd81792d7ed5bee13037d832306f042bd8f58cf
Edward NoaLand <edwardnoaland@gmail.com>
2023-06-12 21:30:06 +0800

    feat: add representational equality test case for chapter 1

--- include/chapter1.h
+++ include/chapter1.h
@@ -1,4 +1,15 @@
 #ifndef ELEMENTS_OF_PROGRAMMING_CHAPTER1_H
 #define ELEMENTS_OF_PROGRAMMING_CHAPTER1_H
 
+struct A {
+    int a;
+    int b;
+    int c;
+};
+
+struct B {
+    int c;
+    int b;
+    int a;
+};
 #endif //ELEMENTS_OF_PROGRAMMING_CHAPTER1_H
